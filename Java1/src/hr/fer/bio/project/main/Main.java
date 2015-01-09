f98ade352a5e53cf36e89ac5ea75dae407c26574
bpervan <branimir.pervan@gmail.com>
2015-01-09 09:59:18 +0100

    Input arguments vs Console readLine

--- Java1/src/hr/fer/bio/project/main/Main.java
+++ Java1/src/hr/fer/bio/project/main/Main.java
@@ -18,31 +18,53 @@ public class Main {
 
     public static final boolean DEBUG = false;
 
+    /**
+     * Usage:
+     * 1. java hr.fer.bio.project.main.Main pathToFastaFile
+     * 2. java hr.fer.bio.project.main.Main pathToFastaFile character endPosition
+     * */
     public static void main(String[] args) throws IOException{
-        Fasta eColi = Fasta.fromFile("d:\\ecoli.fa");
+        if(args.length < 2 || args.length > 4){
+            System.out.println("Arguments?");
+            System.exit(-1);
+        }
+
+        Fasta inputGenome = Fasta.fromFile(args[1]);
         long time1 = System.nanoTime();
-        TreeNode<RRRBlock> rootNode = WaveletTreeBuilder.getInstance().fromStringRRR(eColi.getReadings());
+        TreeNode<RRRBlock> rootNode = WaveletTreeBuilder.getInstance().fromStringRRR(inputGenome.getReadings());
         long time2 = System.nanoTime();
         System.out.println("Wavelet tree with RRR nodes created in " + (time2 - time1) + " ns");
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        String c = " ";
-        String p = " ";
         int resultRank;
-        while(true) {
-            System.out.println("Char: ");
-            c = bufferedReader.readLine();
-            if(c.length() == 0){
-                break;
-            }
-            System.out.println("Position: ");
-            p = bufferedReader.readLine();
+        if(args.length == 4){
             time1 = System.nanoTime();
-            resultRank = rootNode.rank(c.charAt(0), Integer.parseInt(p), rootNode);
+            resultRank = rootNode.rank(args[2].charAt(0), Integer.parseInt(args[3]), rootNode);
             time2 = System.nanoTime();
             System.out.println("Rank: " + resultRank + ". Calculated in " + (time2 - time1) + " ns");
+        } else if(args.length == 2){
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String c = " ";
+            String p = " ";
+
+            while(true) {
+                System.out.println("Char: ");
+                c = bufferedReader.readLine();
+                if(c.length() == 0){
+                    break;
+                }
+                System.out.println("Position: ");
+                p = bufferedReader.readLine();
+                time1 = System.nanoTime();
+                resultRank = rootNode.rank(c.charAt(0), Integer.parseInt(p), rootNode);
+                time2 = System.nanoTime();
+                System.out.println("Rank: " + resultRank + ". Calculated in " + (time2 - time1) + " ns");
+            }
+        } else {
+            System.out.println("Arguments?");
+            System.exit(-1);
         }
+
 
         System.out.println("Over and out");
     }
