d54b1222e944ae6d214b0393c0bab081e2466b95
bkanski <boris.kanski@gmail.com>
2015-01-12 15:58:15 +0100

    Zavrsena klasa RRRTable

--- C++/rrr_table.cc
+++ C++/rrr_table.cc
@@ -1,11 +1,14 @@
 #include "rrr_table.h"
 
+// Constructor
 RRRTable::RRRTable() {
 	CreateTable();
 }
 
+// Destructor
 RRRTable::~RRRTable() {}
 
+// Returns a table instance through it's parameter
 void RRRTable::GetTable(RRRTable** table) {
 	if (*table == instance_)
 		return;
@@ -18,6 +21,8 @@ void RRRTable::GetTable(RRRTable** table) {
 	*table = instance_;
 }
 
+// Decreases a counter of how many clients are using the table
+// If counter reaches 0, then deletes the table instance
 void RRRTable::ReleaseTable(RRRTable** table) {
 	if (*table != instance_)
 		return;
@@ -28,16 +33,18 @@ void RRRTable::ReleaseTable(RRRTable** table) {
 
 	if (counter_ <= 0) {
 		delete instance_;
-		instance_ = 0;
+		instance_ = NULL;
 	}
 
-	*table = 0;
+	*table = NULL;
 }
 
+// Returns block value from RRR table
 uint16_t RRRTable::GetBlock(uint8_t popcount, uint16_t offset) {
 	return table_[popcount][offset];
 }
 
+// Returns index of a block in RRR table
 uint16_t RRRTable::GetOffset(uint8_t popcount, uint16_t block) {
 	int number_of_permutations = table_[popcount].size();
 
@@ -50,6 +57,7 @@ uint16_t RRRTable::GetOffset(uint8_t popcount, uint16_t block) {
 	return 0;
 }
 
+// Generates a table of all 16-bit permutation
 void RRRTable::CreateTable() {
 	for (int popcount = 0; popcount <= 16; ++popcount) {
 		int number_of_permutations = NumberOfPermutations(16, popcount);
@@ -66,12 +74,54 @@ void RRRTable::CreateTable() {
 	}
 }
 
+// Generates next permutation with a given amount of set bits from
+// the previous lexicographical value
 uint16_t RRRTable::NextPermutation(uint16_t value) {
-	//TODO
+	uint16_t mask = 1;
+	int16_t rightmost_bit = -1;
 
-	return 0;
+	for (int i = 0; i < 16; ++i)
+	{
+		if (value & mask)
+		{
+			// remember the index of rightmost set bit
+			if (rightmost_bit == -1)
+				rightmost_bit = (int16_t)i;
+
+			mask = mask << 1;
+
+			// find the rightmost 01 subsequence
+			if (!(value & (mask)))
+			{
+				// change it's value to 10
+				value = value | mask;
+
+				mask = mask >> 1;
+				mask = ~mask;
+				value = value & (mask);
+
+				// shift [0, i] subsequence to the right,
+				// so the rightmost set bit is o index 0
+				mask = (1 << (i + 1));
+				mask = mask - 1;
+				uint16_t right = value & mask;
+				right = right >> rightmost_bit;
+
+				mask = ~mask;
+				value = value & mask;
+				value = value | right;
+
+				return value;
+			}
+		}
+		else
+			mask = mask << 1;
+	}
+
+	return value;
 }
 
+// Returns binomial coefficient(bit_length, popcount)
 uint32_t RRRTable::NumberOfPermutations(uint32_t bit_length, uint32_t popcount) {
 	if (popcount > bit_length) return 0;
 	if (popcount == 0) return 1;
@@ -91,5 +141,8 @@ uint32_t RRRTable::NumberOfPermutations(uint32_t bit_length, uint32_t popcount) 
 	return (uint32_t)(numerator / denominator);
 }
 
-RRRTable* RRRTable::instance_ = 0;
+// Singleton instance of the table
+RRRTable* RRRTable::instance_ = NULL;
+
+// Keeps track of how many clients are using the table
 int RRRTable::counter_ = 0;
--- C++/rrr_table.h
+++ C++/rrr_table.h
@@ -6,35 +6,56 @@
 #include <vector>
 #include <cstdlib>
 
+// This class contains table of all possible 16-bit permutations,
+// for all popcounts (number of bits with value 1). Because there
+// is no need for all RRR sequences in program to have it's own table,
+// this class is implemented using singleton design pattern.
 // Sample usage:
 // RRRTable* table;
 // RRRTable::GetTable(&table);
 // ...
 // RRRTable::ReleaseTable(&table);
-class RRRTable
-{
+class RRRTable {
+	
 public:
+	// Returns an instance of table through it's parameter
 	static void GetTable(RRRTable** table);
 
+	// Must be called at end of usage to properly release allocated
+	// space for table instance
 	static void ReleaseTable(RRRTable** table);
 
+	// Returns block value from the table
 	uint16_t GetBlock(uint8_t popcount, uint16_t offset);
 
+	// Returns index of block in the table
 	uint16_t GetOffset(uint8_t popcount, uint16_t block);
 
 private:
+	// Constructor
 	RRRTable();
 
+	// Destructor
 	~RRRTable();
 
+	// Generates a RRR table
 	void CreateTable();
 
+	// Generates next permutation with a given amount of set bits from
+	// the previous lexicographical value
 	uint16_t NextPermutation(uint16_t value);
 
+	// Returns binomial coefficient(bit_length, popcount)
 	uint32_t NumberOfPermutations(uint32_t bit_length, uint32_t popcount);
 
+	// RRR table. First index is block popcount,
+	// second is it's offset in table
 	std::vector<std::vector<uint16_t> > table_;
+
+	// Singleton instance of the table
 	static RRRTable* instance_;
+
+	// Keeps track of how many clients are using the table
 	static int counter_;
 };
 
