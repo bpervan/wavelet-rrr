66689a9cd6d602249f04422c19bc2018264e2223
bkanski <boris.kanski@gmail.com>
2015-01-12 20:22:28 +0100

    Dodana klasa WTNode i komentari u rrr.h

--- C++/rrr.h
+++ C++/rrr.h
@@ -2,36 +2,70 @@
 #define RRR_H_
 #include <math.h>
 #include <stdint.h>
-
 #include <vector>
 #include "rrr_table.h"
 #include "rrr_block.h"
 
+// This class represents RRR structure
 class RRR {
 
 public:
+	// Constructor
 	RRR(uint32_t sequence_length);
+
+	// Destructor
 	~RRR();
 
+	// Adds a new block in RRR sequence
 	bool AddBlock(uint16_t block, int block_length);
+
+	// Returns a value of bit on index on given index
 	int Access(uint32_t index);
+
+	// Returns number of bits with value 1 from beginning of sequence till
+	// the bit with given index included
 	uint32_t Rank1(uint32_t index);
+
+	// Returns number of bits with value 0 from beginning of sequence till
+	// the bit with given index included
 	uint32_t Rank0(uint32_t index);
+
+	// Returns index of n-th appearance of bit with value 1
 	uint32_t Select1(uint32_t n);
+
+	// Returns index of n-th appearance of bit with value 0
 	uint32_t Select0(uint32_t n);
 
+	// Returns length of bitmap encoded with RRR
 	uint32_t length();
+
+	// Returns number of bits inside one RRR block
 	int bits_per_block();
+
+	// Returns number of blocks inside one RRR superblock
 	int blocks_per_superblock();
 
 private:
+	// RRR blocks
 	std::vector<RRRBlock> blocks_;
+
+	// RRR superblocks
 	std::vector<uint32_t> superblocks_;
+
+	// Instance of RRR table
 	RRRTable* table_;
 
+	// Length of bitmap encoded with RRR
 	uint32_t length_;
+
+	// Number of bits in one RRR block
 	int bits_per_block_;
+
+	// Number of blocks in one RRR superblock
 	int blocks_per_superblock_;
+
+	// Length of the last block in RRR sequence
+	// (it could be less than bits_per_block)
 	int last_block_length_;
 };
 
---
+++ C++/wavelet_tree_node.h
@@ -0,0 +1,73 @@
+#ifndef WAVELET_TREE_NODE_H_
+#define WAVELET_TREE_NODE_H_
+#include <stdint.h>
+
+#include <string>
+#include "rrr.h"
+
+// This class represents interface for nodes in wavelet tree
+class WTNode
+{
+public:
+	// Destructor
+	virtual ~WTNode() = 0;
+
+	// Returns character on index which is given as parameter
+	virtual char GetCharAt(uint32_t index) = 0;
+
+	// Sets left child of the node
+	virtual void SetLeftChild(WTNode* node) = 0;
+
+	// Sets right child of the node
+	virtual void SetRightChild(WTNode* node) = 0;
+
+	// Returns number of appearance of character given as parameter from beggining
+	// of string till the index specified as second parameter included
+	virtual uint32_t GetCharRank(	char character,
+	 															uint32_t index,
+																std::string alphabet) = 0;
+};
+
+// This class represents internal node in wavelet tree. It contains it's
+// RRR sequence and pointers to it's left and right child.
+class WTInternalNode : public WTNode
+{
+public:
+	WTInternalNode(RRR* rrr);
+	virtual ~WTInternalNode();
+	virtual char GetCharAt(uint32_t index);
+	virtual void SetLeftChild(WTNode* node);
+	virtual void SetRightChild(WTNode* node);
+	virtual uint32_t GetCharRank(	char character,
+																uint32_t index,
+																std::string alphabet);
+
+private:
+	// RRR sequence
+	RRR* rrr_sequence_;
+
+	// Left child
+	WTNode* left_;
+
+	// Right child
+	WTNode* right_;
+};
+
+// This class represents leaf node in wavelet tree. It contains only one
+// character of the alphabet from the input string
+class WTLeafNode : public WTNode
+{
+public:
+	WTLeafNode(char character);
+	virtual ~WTLeafNode();
+	virtual char GetCharAt(uint32_t index);
+	virtual void SetLeftChild(WTNode* node);
+	virtual void SetRightChild(WTNode* node);
+	virtual uint32_t GetCharRank(char character, uint32_t index, std::string alphabet);
+
+private:
+	// Character
+	char character_;
+};
+
+#endif /* WAVELET_TREE_NODE_H_ */
