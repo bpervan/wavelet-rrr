f50a894c1ce0b5fdde91e6fd9cbc74922dce92c5
bpervan <branimir.pervan@gmail.com>
2015-01-13 22:13:45 +0100

    Rankable extends TreeOperations

--- Java1/src/hr/fer/bio/project/rankable/TreeOperations.java
+++ Java1/src/hr/fer/bio/project/rankable/TreeOperations.java
@@ -6,6 +6,6 @@ import hr.fer.bio.project.wavelet.TreeNode;
  * Created by Branimir on 12.1.2015..
  */
 public interface TreeOperations {
-    /*public int rank(char c, int endPos, TreeNode rootNode);
-    public int select(char c, int boundary, TreeNode rootNode);*/
+    public int rank(char c, int endPos, TreeNode rootNode);
+    public int select(char c, int boundary, TreeNode rootNode);
 }
