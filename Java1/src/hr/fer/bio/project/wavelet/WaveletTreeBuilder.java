42439cfa67c8709ebaf19b609162660b7585af3e
bpervan <branimir.pervan@gmail.com>
2015-01-14 17:52:46 +0100

    Added rest of the comments

--- Java1/src/hr/fer/bio/project/wavelet/WaveletTreeBuilder.java
+++ Java1/src/hr/fer/bio/project/wavelet/WaveletTreeBuilder.java
@@ -9,19 +9,44 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by Branimir on 21.10.2014..
+ * Builder class for WaveletTree. This class builds Wavelet tree from input string sequence.
+ * At this moment, separate methods for building trees with BooleanArray nodes and with RRRBlock
+ * nodes are used
+ *
+ * @see hr.fer.bio.project.wavelet.TreeNode
+ * @see hr.fer.bio.project.booleanarray.BooleanArray
+ * @see hr.fer.bio.project.rrr.RRRBlock
+ *
+ * @author bpervan
+ * @since 1.0
  */
 public class WaveletTreeBuilder {
 
     private WaveletTreeBuilder(){}
+
+    /** Factory method */
     public static WaveletTreeBuilder getInstance(){
         return new WaveletTreeBuilder();
     }
 
+    /**
+     * Delegates method call to internal/private method
+     * */
     public TreeNode<BooleanArray> fromString(String string){
         return fromString(string, toMap(string), 0, string.length() - 1);
     }
 
+    /**
+     * Recursively builds Wavelet tree from input string sequence
+     * Node is BooleanArray
+     *
+     * @param string Input sequence
+     * @param charMap Character -> Boolean mappings
+     * @param min Left bound
+     * @param max Right bound
+     *
+     * @return Tree root
+     * */
     private TreeNode<BooleanArray> fromString(String string, Map<Character, Boolean> charMap, int min, int max){
         if(charMap.size() < 2){
             return null;
@@ -40,11 +65,24 @@ public class WaveletTreeBuilder {
         return tempnode;
     }
 
-    //TEST TEST TEST
+    /**
+     * Delegates method call to internal/private method
+     * */
     public TreeNode<RRRBlock> fromStringRRR(String string){
         return fromStringRRR(string, toMap(string), 0, string.length() - 1);
     }
-    //TEST TEST TEST
+
+    /**
+     * Recursively builds Wavelet tree from input string sequence
+     * Node is RRRBlock
+     *
+     * @param string Input sequence
+     * @param charMap Character -> Boolean mappings
+     * @param min Left bound
+     * @param max Right bound
+     *
+     * @return Tree root
+     * */
     private TreeNode<RRRBlock> fromStringRRR(String string, Map<Character, Boolean> charMap, int min, int max){
         if(charMap.size() < 2){
             return null;
@@ -62,11 +100,22 @@ public class WaveletTreeBuilder {
         }
         return tempnode;
     }
-    //TEST TEST TEST
+
+    /**
+     * Adapter for encodeString method call, used for building RRRBlock
+     * */
     private RRRBlock encodeStringRRR(String string, Map<Character, Boolean> charMap){
         return new RRRBlock(encodeString(string, charMap));
     }
 
+    /**
+     * Divides string based on mapping in charMap
+     *
+     * @param string string for division
+     * @param charMap mappings
+     *
+     * @return divided string as string array (on indices 0 and 1)
+     * */
     private String[] stringDivider(String string, Map<Character, Boolean> charMap){
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
@@ -85,7 +134,14 @@ public class WaveletTreeBuilder {
         return returnArray;
     }
 
-
+    /**
+     * Encodes string as BooleanArray
+     *
+     * @param string string to be encoded
+     * @param charMap mappings
+     *
+     * @return BooleanArray representing encoded string
+     * */
     private BooleanArray encodeString(String string, Map<Character, Boolean> charMap){
         BooleanArray booleanArray = new BooleanArray(string.length());
         for(int i = 0; i < string.length(); ++i){
@@ -95,6 +151,13 @@ public class WaveletTreeBuilder {
         return booleanArray;
     }
 
+    /**
+     * Extracts mapping(dictionary) from input string
+     *
+     * @param string input string
+     *
+     * @return HashMap with mappings between chars in input string and booleans
+     * */
     private Map<Character, Boolean> toMap(String string){
         List<Character> charList = extractAlphabet(string);
         Map<Character, Boolean> charMap = new HashMap<Character, Boolean>();
@@ -109,11 +172,26 @@ public class WaveletTreeBuilder {
         return charMap;
     }
 
-
+    /**
+     * Chechs if char is present in a list of chars
+     *
+     * @param haystack list of chars
+     * @param needle char we are looking for
+     * @param low left bound
+     * @param high right bound
+     *
+     * @return true if char needle is present in haystack surrounded by left and right bound false otherwise
+     * */
     private boolean isInArray(List<Character> haystack, char needle, int low, int high){
         return haystack.subList(low, high).contains(needle);
     }
 
+    /**
+     * Extracts alphabet from input string
+     *
+     * @param s input string
+     * @return List of characters from extracted string
+     * */
     private List<Character> extractAlphabet(String s){
         List<Character> charList = new ArrayList<Character>();
         for(char c : s.toCharArray()){
